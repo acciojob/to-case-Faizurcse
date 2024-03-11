01f4b60077b8a60cc86319e103825cad37d67382
Md FaizurRahman <114854264+Faizurcse@users.noreply.github.com>
2024-03-12 02:56:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function toCase(text) {
-  // write your code here
+  let u = text.toUpperCase();
+  
+  // Convert text to lowercase
+  let l = text.toLowerCase();
+
+  // Return the combined string
+  return `${l}-${u}`;
 }
 
 // DO not change the code below
